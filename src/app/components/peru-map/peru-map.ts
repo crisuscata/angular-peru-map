53a92f0f1003f9fbf7180c9ce124b1bc7a315177
Desarrollo tres <ingsof01@sededefensoria.gob.pe>
2025-10-30 14:18:25 -0500

    adding number and icon

--- src/app/components/peru-map/peru-map.ts
+++ src/app/components/peru-map/peru-map.ts
@@ -23,64 +23,45 @@ export class PeruMapComponent implements AfterViewInit {
   private initMap(): void {
     const L = this.L;
 
-    // Crear mapa base centrado en Perú
+    // Crear mapa base centrado en Perú (Lima)
     this.map = L.map('map', {
-      center: [-9.19, -75.0152],
-      zoom: 6,
+      center: [-12.0464, -77.0428], // Coordenadas de Lima
+      zoom: 12,
       zoomControl: true,
     });
 
-    // Capa base (OpenStreetMap)
+    // Capa base
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors',
     }).addTo(this.map);
 
-    // 📍 Definir íconos personalizados
-    const redIcon = L.icon({
-      iconUrl: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
-      popupAnchor: [0, -30],
+    // 📍 Datos de pines personalizados con coordenadas exactas
+    const markers = [
+      { coords: [-12.0464, -77.0428], color: 'red', number: 2034588 }, // Lima Centro
+      { coords: [-12.03, -77.05], color: 'yellow', number: 2033741 },   // Miraflores
+      { coords: [-12.06, -77.08], color: 'green', number: 2036426 },    // San Isidro
+      { coords: [-12.05, -77.03], color: 'red', number: 2053749 },      // Barranco
+    ];
+
+    // 📍 Crear y agregar pines con íconos y números personalizados
+    markers.forEach((m) => {
+      const html = `
+        <div class="custom-pin ${m.color}">
+          <img src="https://maps.google.com/mapfiles/ms/icons/${m.color}-dot.png" class="pin-icon" />
+          <div class="pin-label">${m.number}</div>
+        </div>
+      `;
+
+      const icon = L.divIcon({
+        className: '', // Limpiar la clase por defecto
+        html: html,
+        iconSize: [32, 40], // Ajustar el tamaño general del ícono y número
+        iconAnchor: [16, 32], // Centrar el ícono en el marcador
+        popupAnchor: [0, -40], // Ajustar el popup para mostrar el número correctamente
+      });
+
+      // Crear el marcador con el ícono y el número
+      L.marker(m.coords, { icon }).addTo(this.map);
     });
-
-    const blueIcon = L.icon({
-      iconUrl: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
-      popupAnchor: [0, -30],
-    });
-
-    const greenIcon = L.icon({
-      iconUrl: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
-      popupAnchor: [0, -30],
-    });
-
-    const purpleIcon = L.icon({
-      iconUrl: 'https://maps.google.com/mapfiles/ms/icons/purple-dot.png',
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
-      popupAnchor: [0, -30],
-    });
-
-    // 📍 Agregar pines en Lima
-    const limaCoords = [-12.0464, -77.0428];
-
-    L.marker(limaCoords, { icon: redIcon })
-      .addTo(this.map)
-      .bindPopup('<b>Lima Centro</b>');
-
-    L.marker([-12.03, -77.05], { icon: blueIcon })
-      .addTo(this.map)
-      .bindPopup('<b>Miraflores</b>');
-
-    L.marker([-12.06, -77.08], { icon: greenIcon })
-      .addTo(this.map)
-      .bindPopup('<b>San Isidro</b>');
-
-    L.marker([-12.05, -77.03], { icon: purpleIcon })
-      .addTo(this.map)
-      .bindPopup('<b>Barranco</b>');
   }
 }
